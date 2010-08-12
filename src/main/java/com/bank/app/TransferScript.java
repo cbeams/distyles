99d81a6a510dbd2e33a8f54f4a86112e75db4e34
Chris Beams <cbeams@gmail.com>
2010-08-12 15:02:45 +0200

    More renaming

--- src/main/java/com/bank/app/TransferScript.java
+++ src/main/java/com/bank/app/TransferScript.java
@@ -12,8 +12,8 @@ public class TransferScript {
 	public static void main(String... args) throws InsufficientFundsException {
 
 		ApplicationContext ctx =
-			new ClassPathXmlApplicationContext("/com/bank/config/transfer-xml-config.xml");
-//			new ClassPathXmlApplicationContext("/com/bank/config/transfer-annotation-config.xml");
+//			new ClassPathXmlApplicationContext("/com/bank/config/transfer-xml-config.xml");
+			new ClassPathXmlApplicationContext("/com/bank/config/transfer-annotation-config.xml");
 //			new AnnotationConfigApplicationContext(TransferConfig.class);
 		
 
